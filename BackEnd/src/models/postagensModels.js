f4c8ca296dcf7214ddb3b7e1407120218c19d769
Maria Eduarda <eduardacampos652@gmail.com>
2024-09-06 08:20:56 -0300

    RF01 finalizada

---
+++ BackEnd/src/models/postagensModels.js
@@ -0,0 +1,36 @@
+import conn from '../config/conn.js';
+import { DataTypes } from 'sequelize';
+
+const Postagem = conn.define("postagens", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    titulo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+    },
+    conteudo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+    },
+    dataPublicacao: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        autoIncrement: true
+    },
+    autor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+    }
+},
+    {
+        tableName: "postagens"
+    }
+);
+
+export default Postagem
